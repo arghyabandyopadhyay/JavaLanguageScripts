8ae42c0467ac28200de1dbc087c742ba55c273b9
arghyabandyopadhyay <arghyab13bga@gmail.com>
2021-03-16 22:05:38 +0530

    StoogeSort update

--- AlgorithmsInJava/SortingAlgorithms/StoogeSort.java
+++ AlgorithmsInJava/SortingAlgorithms/StoogeSort.java
@@ -2,18 +2,19 @@
 import java.io.*; 
 
 public class StoogeSort { 
+	int arrayToBeSorted[];
 	// Function to implement stooge sort 
-	static void stoogesort(int arr[], int l, int h) 
+	void stoogesort(int l, int h) 
 	{ 
 		if (l >= h) 
 			return; 
 
 		// If first element is smaller 
 		// than last, swap them 
-		if (arr[l] > arr[h]) { 
-			int t = arr[l]; 
-			arr[l] = arr[h]; 
-			arr[h] = t; 
+		if (arrayToBeSorted[l] > arrayToBeSorted[h]) { 
+			int t = arrayToBeSorted[l]; 
+			arrayToBeSorted[l] = arrayToBeSorted[h]; 
+			arrayToBeSorted[h] = t; 
 		} 
 
 		// If there are more than 2 elements in 
@@ -22,27 +23,28 @@ public class StoogeSort {
 			int t = (h - l + 1) / 3; 
 
 			// Recursively sort first 2/3 elements 
-			stoogesort(arr, l, h - t); 
+			stoogesort(l, h - t); 
 
 			// Recursively sort last 2/3 elements 
-			stoogesort(arr, l + t, h); 
+			stoogesort(l + t, h); 
 
 			// Recursively sort first 2/3 elements 
 			// again to confirm 
-			stoogesort(arr, l, h - t); 
+			stoogesort(l, h - t); 
 		} 
 	} 
 
 	// Driver Code 
 	public static void main(String args[]) 
 	{ 
-		int arr[] = { 2, 4, 5, 3, 1 }; 
-		int n = arr.length; 
-
-		stoogesort(arr, 0, n - 1); 
-
-		for (int i = 0; i < n; i++) 
-			System.out.print(arr[i] + " "); 
+		StoogeSort m=new StoogeSort();
+		m.arrayToBeSorted=new int[args.length];
+		for (int j=0;j<args.length;j++)m.arrayToBeSorted[j]=Integer.parseInt(args[j]);
+		m.stoogesort(0,args.length-1); 
+		String output="[";
+		for (int j=0;j<m.arrayToBeSorted.length;j++)output+=m.arrayToBeSorted[j]+",";
+		output=output.substring(0,output.length()-1)+"]";
+		System.out.println(output);
 	} 
 } 
 // Code Contributed by Mohit Gupta_OMG <(0_o)> 
