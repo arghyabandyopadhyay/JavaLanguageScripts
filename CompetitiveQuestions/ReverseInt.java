6d1c617747547471a35820f6afcaffb509575cfa
arghyabandyopadhyay <arghyab13bga@gmail.com>
2021-07-15 16:14:56 +0530

    Palindrome and Reverse String

--- CompetitiveQuestions/ReverseInt.java
+++ CompetitiveQuestions/ReverseInt.java
@@ -1,9 +1,10 @@
-class reverseString{
+
+class ReverseString{
 	public static void main(String args[]){
-		String rev=ReverseString(args[0]);
+		String rev=reverseString(args[0]);
 		System.out.println(rev);
 	}
-	public static String ReverseString(String str){
+	String reverseString(String str){
 		String a="";
 		for(int i=0;i<str.length();i++)a=str.charAt(i)+a;
 		return a;
