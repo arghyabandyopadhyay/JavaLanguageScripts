22144dc1d27c2c0ef332f620a2c6146a6e630d2d
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2023-09-14 19:46:08 +0530

    Create LargestRectangleInHistogram.java

---
+++ CompetitiveQuestions/LargestRectangleInHistogram.java
@@ -0,0 +1,18 @@
+class LargestRectangleInHistogram {
+    public int largestRectangleArea(int[] heights) {
+        int maxArea=0;
+        Stack<Integer> stack=new Stack<Integer>();
+        stack.push(0);
+        for(int i=1;i<=heights.length;i++){
+            int curr=(i==heights.length)?-1:heights[i];
+            while(!stack.isEmpty()&&curr<=heights[stack.peek()]){
+                int height=heights[stack.pop()];
+                int width=stack.isEmpty()?i:i-stack.peek()-1;
+                int currArea=height*width;
+                maxArea=Math.max(maxArea,currArea);
+            }
+            stack.push(i);
+        }
+        return maxArea;
+    }
+}
